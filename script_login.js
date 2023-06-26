b5078684f93c6b2a94b0c5ab57ef628f377cc7c1
Kent <127605021+KentBrian054@users.noreply.github.com>
2023-06-26 11:51:18 +0800

    add another
    adding addtocart

--- script_login.js
+++ script_login.js
@@ -1,38 +1,227 @@
-// Get references to the input elements and error messages
-const emailInput = document.querySelector('#email');
-const emailError = document.querySelector('#emailError');
-const passwordInput = document.querySelector('#password');
-const passwordError = document.querySelector('#passwordError');
+const firstname = document.querySelector('#firstname');
+const lastname = document.querySelector('#lastname');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const input = document.querySelectorAll('input');
 
-// Add an event listener to the form's submit event
-document.querySelector('.container-login').addEventListener('submit', (event) => {
-  // Prevent the form from being submitted
-  event.preventDefault();
+// forms
+const signupForm = document.querySelector('#signup-form');
+const loginForm = document.querySelector('#login-form');
 
-  // Clear any previous error messages
-  emailError.textContent = '';
-  passwordError.textContent = '';
+// errors
+const fnameError = document.getElementById('fnameError');
+const lnameError = document.getElementById('lnameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
+const message = document.querySelector('span');
 
-  // Validate the email input
-  if (!emailInput.value) {
-    emailError.textContent = 'Email is required';
-  } else if (!emailInput.value.includes('@')) {
-    emailError.textContent = 'Email must be a valid email address';
-  }
+// button
+const logoutBtn = document.getElementById('logout');
 
-  // Validate the password input
-  if (!passwordInput.value) {
-    passwordError.textContent = 'Password is required';
-  } else if (passwordInput.value.length < 8) {
-    passwordError.textContent = 'Password must be at least 8 characters long';
-  }
 
-  // If there are no errors, submit the form data
-  if (!emailError.textContent && !passwordError.textContent) {
-    // Submit the form data here
-    console.log('Form submitted:', {
-      email: emailInput.value,
-      password: passwordInput.value,
-    });
-  }
+// user signup
+if (signupForm) {
+
+  signupForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const firstNameValue = firstname.value.trim();
+    const lastNameValue = lastname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    const min = 2;
+
+    if (firstNameValue === '') {
+      fnameError.innerText = 'Firstname is required.'
+      fnameError.classList.add('error');
+      firstname.style.border = '2px solid red';
+
+    } else if (firstNameValue.length < min) {
+      fnameError.innerText = 'Minimum length required is 2.';
+      fnameError.classList.add('error');
+      firstname.style.border = '2px solid red';
+
+    } else if (!validName(firstNameValue)) {
+      fnameError.innerText = 'Firstname is not valid.';
+      fnameError.classList.add('error');
+      firstname.style.border = '2px solid red';
+
+    } else {
+      localStorage.setItem('firstname', firstNameValue);
+      fnameError.innerText = '';
+      firstname.style.border = '2px solid green';
+    }
+
+
+    if (lastNameValue === '') {
+      lnameError.innerText = 'Lastname is required.';
+      lnameError.classList.add('error');
+      lastname.style.border = '2px solid red';
+
+    } else if (lastNameValue.length < 2) {
+      lnameError.innerText = 'Minimum length required is 2.';
+      lnameError.classList.add('error');
+      lastname.style.border = '2px solid red';
+
+    } else if (!validName(lastNameValue)) {
+      lnameError.innerText = 'Lastname is not valid.';
+      lnameError.classList.add('error');
+      firstname.style.border = '2px solid red';
+
+    } else {
+      localStorage.setItem('lastname', lastNameValue);
+      lnameError.innerText = '';
+      lastname.style.border = '2px solid green';
+    }
+
+
+    if (emailValue === '') {
+      emailError.innerText = 'Email Address is required.';
+      emailError.classList.add('error');
+      email.style.border = '2px solid red';
+
+    } else if (!validEmail(emailValue)) {
+      emailError.innerText = 'Email Address is not valid.';
+      emailError.classList.add('error');
+      email.style.border = '2px solid red';
+
+    } else {
+      localStorage.setItem('email', emailValue);
+      emailError.innerText = '';
+      email.style.border = '2px solid green';
+    }
+
+    
+    if (passwordValue === '') {
+      passwordError.innerText = 'Password is required.';
+      passwordError.classList.add('error');
+      password.style.border = '2px solid red';
+
+    } else if (!validPassword(passwordValue)) {
+      passwordError.innerText = 'Minimum length required is 8, at least one letter, \n one number, and one special character.';
+      passwordError.classList.add('error');
+      password.style.border = '2px solid red';
+
+    } else {
+      localStorage.setItem('password', passwordValue);
+      passwordError.innerText = '';
+      password.style.border = '2px solid green';
+    }
+
+
+    const getFirstName = localStorage.getItem('firstname');
+    const getLastName = localStorage.getItem('lastname');
+    const getEmail = localStorage.getItem('email');
+    const getPassword = localStorage.getItem('password');
+
+
+    const getSignup = getFirstName && getLastName && getEmail && getPassword;
+    const getSignupValue = firstNameValue && lastNameValue && emailValue && passwordValue;
+
+
+    if (getSignupValue === '') {
+      message.innerText = 'All fields are required.';
+      message.classList.remove('success-message');
+      message.classList.add('error-message');
+    } else if (getSignup != null) {
+      message.innerText = 'Account successfully created.';
+      message.classList.remove('error-message');
+      message.classList.add('success-message');
+
+      for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+      }
+
+    }
+  });
+}
+
+
+// regular expressions
+function validName(name) {
+  const re = /^[a-zA-Z]+$/;
+  return re.test(name);
+}
+
+function validEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validPassword(password) {
+  const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return re.test(password);
+}
+
+
+//user login
+if (loginForm) {
+
+  loginForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+  
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    const getEmailAddress = localStorage.getItem('email');
+    const getPasword = localStorage.getItem('password');
+    
+    
+    if (emailValue === getEmailAddress && passwordValue === getPasword) {
+      location.href = 'member-page.html';
+
+    } else if (emailValue === '' || passwordValue === '') {
+      message.innerText = 'Email address and password are required.';
+      message.classList.add('error-message');
+      email.style.border = '2px solid red';
+      password.style.border = '2px solid red';
+
+    } else if (!validEmail(emailValue)) {
+      message.innerText = 'Email address is not valid.';
+      message.classList.add('error-message');
+
+    } else {
+      message.innerText = 'Invalid email address or password.';
+      message.classList.add('error-message');
+      email.style.border = '2px solid red';
+      password.style.border = '2px solid red';
+    }
+  });
+
+}
+
+// user logout
+if (logoutBtn) {
+
+  const firstname = document.getElementById('firstname');
+
+  firstname.innerText = localStorage.getItem('firstname');
+  firstname.style.color = 'orange';
+
+  logoutBtn.addEventListener('click', function() {
+    location.replace('index.html');
+  });
+
+}
+
+
+// responsive menu 
+const links = document.querySelector('.nav-links');
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+
+openMenu.addEventListener('click', function() {
+  links.style.display = 'flex';
+  links.style.top = '0';
 });
+
+closeMenu.addEventListener('click', function() {
+  links.style.top = '-100%';
+});
+
+
+function log() {
+  console.log(email)
+}
